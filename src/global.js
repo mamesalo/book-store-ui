ec4eb793bde71ccb37a26c9da78915f1662bd59c
mamesalo <117835018+mamesalo@users.noreply.github.com>
2025-01-16 18:14:09 +0300

    Update global.js

--- src/global.js
+++ src/global.js
@@ -1,2 +1,2 @@
-export const SERVER_URL = "http://localhost:5555";
-// export const SERVER_URL = "https://dot-bookstore-app-server.vercel.app";
+// export const SERVER_URL = "http://localhost:5555";
+export const SERVER_URL = "https://book-store-backend-livid-sigma.vercel.app";
